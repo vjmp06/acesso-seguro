194ef4403ca9e37354860360c5b0984bf5e5dba2
Vinicius Moreira <vinicius_moreira06@hotmail.com>
2020-07-05 23:51:37 -0300

    Atualizações necessárias

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,21 +1,10 @@
-import express, { response } from 'express';
-import { celebrate, Joi} from 'celebrate';
+import express from 'express';
 
-import multer from 'multer';
-import multerConfig from './config/multer';
-
-import PointsController from './controllers/PointsController';
-import ItemsController from './controllers/ItemsController';
 import AccessController from './controllers/AccessController';
 import UsersController from './controllers/UsersController';
 
-//index, show, create ,update, delete
-
 const routes = express.Router();
-const upload = multer(multerConfig);
 
-const pointsController = new PointsController();
-const itemsController = new ItemsController();
 const usersController = new UsersController();
 const accessController = new AccessController();
 
@@ -26,32 +15,6 @@ routes.get('/users/:id', usersController.show);
 routes.get('/access_control', accessController.index);
 routes.post('/access_control', accessController.create);
 
-routes.get('/items', itemsController.index);
 
-routes.get('/points', pointsController.index);
-routes.get('/points/:id', pointsController.show);
-
-routes.post(
-  '/points', 
-  upload.single('image'),
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required(),
-      email: Joi.string().required(),
-      whatsapp: Joi.number().required(),
-      latitude: Joi.number().required(),
-      longitude: Joi.number().required(),
-      city: Joi.string().required(),
-      uf: Joi.string().required().max(2),
-      items: Joi.string().required(),
-    })
-  }, {
-    abortEarly: false
-  }),
-  pointsController.create
-);
 
 export default routes;
-
-//Service Pattern
-//Repostiroy Pattern (Data Mapper)
